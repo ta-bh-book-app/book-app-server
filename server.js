1f48d472a33a4448ec70039fbae519ed8889bd45
tganyan <tganyan@gmail.com>
2018-07-24 12:11:14 -0700

    adding event listener for connection errors

--- server.js
+++ server.js
@@ -3,6 +3,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
+const conString = process.env.DATABASE_URL;
+const client = new pg.Client(conString);
+
+client.connect();
+client.on('error', error => {
+  console.error(error);
+});
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
